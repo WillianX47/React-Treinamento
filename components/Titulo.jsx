74f10c08850ca22573b17995be5fe22348e8c8fb
WillianX47 <willianx47@gmail.com>
2021-12-11 17:51:29 -0300

    [Feature Will - Task: Update Titulo | 11.12.21]

--- components/Titulo.jsx
+++ components/Titulo.jsx
@@ -1,8 +1,13 @@
-export default function Titulo(){
-    return (
-        <>
-            <h1>Título</h1>
-            <h2>Subtitulo</h2>
-        </>
-    )
+export default function Titulo(props) {
+  return props.pequeno ? (
+    <>
+      <p>{props.principal}</p>
+      <p>{props.secundario}</p>
+    </>
+  ) : (
+    <>
+      <h1>{props.principal}</h1>
+      <h2>{props.secundario}</h2>
+    </>
+  );
 }
--- pages/basicos/usandoTitulo.jsx
+++ pages/basicos/usandoTitulo.jsx
@@ -1,10 +1,14 @@
-import Titulo from '../../components/Titulo'
+import Titulo from "../../components/Titulo";
 
-export default function usandoTitulo(){
-    return (
-        <div>
-            <h1>Usando titulo</h1>
-            <Titulo></Titulo>
-        </div>
-    )
+export default function usandoTitulo() {
+  return (
+    <div>
+      <Titulo
+        principal="Página de cadastro com letras pequenas"
+        secundario="Incluir, alterar e excluir coisas! com letra pequena"
+        pequeno={true}
+      ></Titulo>
+      <Titulo principal="Página de cadastro" secundario="Incluir, alterar e excluir coisas!"></Titulo>
+    </div>
+  );
 }
