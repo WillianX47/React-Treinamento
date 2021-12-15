11ded4fdddd740a10c134537b802eb5007c7bada
WillianX47 <willianx47@gmail.com>
2021-12-15 01:17:21 -0300

    [Feature Will - Task: style | 15.12.21]

---
+++ pages/css/usandoEstilo.jsx
@@ -0,0 +1,8 @@
+import Estilo from "../../components/Estilo";
+
+export default function usandoEstilo(){
+    return <div>
+        <Estilo numero={3} color="#fff"/>
+        <Estilo numero={-12} color="#000" direita/>
+    </div>
+}
