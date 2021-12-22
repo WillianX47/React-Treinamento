28f6a65b1b804bb72ca258f61565c050b2ad81ee
WillianX47 <willianx47@gmail.com>
2021-12-21 22:16:42 -0300

    [Feature Will - Task: Utilizando componentes filhos | 21.12.21]

---
+++ components/ListaItem.jsx
@@ -0,0 +1,11 @@
+export default function ListaItem(props) {
+  return (
+    <div>
+      <h1>Lista de algo</h1>
+      <ul style={{
+          listStyle: "none",
+          padding: 0
+      }}>{props.children}</ul>
+    </div>
+  );
+}
---
+++ pages/basicos/usandoElementoFilho.jsx
@@ -0,0 +1,12 @@
+import ListaItem from "../../components/ListaItem";
+import Item from "../../components/Item";
+
+export default function usandoElementoFilho() {
+  return (
+    <div>
+      <ListaItem>
+        <Item conteudo="Teste"></Item>
+      </ListaItem>
+    </div>
+  );
+}
