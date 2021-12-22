342c3a1dc5e2792ed48d49cff8f74c2f2b048749
WillianX47 <willianx47@gmail.com>
2021-12-21 21:33:54 -0300

    [Feature Will - Task: separando em função | 21.12.21]

--- components/SomentePar.jsx
+++ components/SomentePar.jsx
@@ -1,7 +1,11 @@
 export default function SomentePar(props) {
   const numeroPar = props.numero % 2 === 0;
 
-  return <div>{numeroPar ? <span>{props.numero}</span> : null}</div>;
+  return <div>{numeroPar ? renderizaPar(props.numero) : null}</div>;
+
+  function renderizaPar(props){
+      return <span>{props}</span>
+  }
 
   // if(props.numero % 2 === 0){
   //     return <span>{props.numero}</span>
