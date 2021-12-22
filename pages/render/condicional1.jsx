59e2f157de964535731af8042d5b0f128a565c13
WillianX47 <willianx47@gmail.com>
2021-12-21 21:31:24 -0300

    [Feature Will - Task: condicional1 | 21.12.21]

---
+++ pages/render/condicional1.jsx
@@ -0,0 +1,9 @@
+import SomentePar from "../../components/SomentePar";
+
+export default function condicional1() {
+  return (
+    <div>
+      <SomentePar numero={10}></SomentePar>,<SomentePar numero={11}></SomentePar>,<SomentePar numero={12}></SomentePar>
+    </div>
+  );
+}
