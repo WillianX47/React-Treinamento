8869826d2eb354f4a889400c023ef421cc2a7297
WillianX47 <willianx47@gmail.com>
2022-01-12 23:37:49 -0300

    [Feature Will - Task: Estilizar contador | 12.01.22]

---
+++ components/ContadorDisplay.jsx
@@ -0,0 +1,18 @@
+export default function ContadorDisplay(props){
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100px",
+            width: "100px",
+            borderRadius: "50px",
+            backgroundColor: "#222",
+            color: "white",
+            fontSize: "2rem",
+            margin: "20px"
+        }}>
+            {props.numero}
+        </div>
+    )
+}
--- pages/estado/contador.jsx
+++ pages/estado/contador.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
+import ContadorDisplay from "../../components/ContadorDisplay";
 
-export default function contador(){
-    const [x, setX] = useState(0)
+export default function contador() {
+  const [x, setX] = useState(0);
 
-    const incrementa = () => setX(x+1)
-    const decrementa = () => setX(x-1)
+  const incrementa = () => setX(x + 1);
+  const decrementa = () => setX(x - 1);
 
-    return (
-        <div>
-            <h1>Contador</h1>
-            <div>Valor: {x}</div>
-            <button onClick={decrementa}>-</button>
-            <button onClick={incrementa}>+</button>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Contador</h1>
+      <ContadorDisplay numero={x}></ContadorDisplay>
+      <div>
+      <button onClick={decrementa}>-</button>
+      <button onClick={incrementa}>+</button>
+      </div>
+    </div>
+  );
 }
